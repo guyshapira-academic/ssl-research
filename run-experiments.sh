7cc5435576cfb2f8f211a549f7402b020c91d26e
Guy Shapira <guyshapira3@mail.tau.ac.il>
2023-08-29 11:18:17 +0300

    bugfix

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3.10 train.py -m model.type=vanilla,resnet width_factor=0.5,1.0,1.25,1.5 depth_factor=0.5,1,1.5,2
+python3.10 train.py -m model.type=vanilla,resnet model.width_factor=0.5,1.0,1.25,1.5 model.depth_factor=0.5,1,1.5,2
