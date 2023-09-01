fb2c62859fe5af8b88fff6c19f8377f62eccdc4b
Guy Shapira <guyshapira3@mail.tau.ac.il>
2023-09-01 10:32:19 +0300

    Update command

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3.10 train.py -m model.type=vanilla,resnet model.width_factor=1.0,1.25,1.5 model.depth_factor=1,1.5,2
+python3.10 train.py -m model.type=resnet model.width_factor=1.5 model.depth_factor=2.0
+python3.10 train.py -m model.type=vanilla model.width_factor=1.5 model.depth_factor=1.0,2.0
+python3.10 train.py -m model.type=vanilla model.width_factor=1.0 model.depth_factor=2.0
